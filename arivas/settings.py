c54a24cfb45089b5a99735558284b920935d55e5
sa001gar <kundusagar233@gmail.com>
2025-09-18 23:21:11 +0530

    fix: Restore usage of CompressedStaticFilesStorage for static file handling

--- arivas/settings.py
+++ arivas/settings.py
@@ -197,8 +197,8 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        # "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        # "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
